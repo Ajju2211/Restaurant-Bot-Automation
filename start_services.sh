8bdd674bf60a7fa7641b65d735c8bd8a089357f1
Ajju2211 <azharuddinm2211@gmail.com>
2020-08-07 01:38:40 +0530

    changed endpoint

--- start_services.sh
+++ start_services.sh
@@ -1,4 +1,9 @@
 cd app/
 # Start rasa server with nlu model
-rasa run --model models --enable-api --cors "*" --debug \
-         -p $PORT
+rasa run actions &
+rasa run --model /app/models --enable-api \
+        --endpoints endpoints.yml \
+        --credentials credentials.yml \
+        -p $PORT
+# rasa run --model models --enable-api --cors "*" --debug \
+#          -p $PORT
