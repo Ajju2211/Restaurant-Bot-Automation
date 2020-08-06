aa77ef9328600d625014bcda9911cbfaca70f8f6
Ajju2211 <azharuddinm2211@gmail.com>
2020-08-06 16:46:31 +0530

    actionserver updated

--- actionserver/start_services.sh
+++ actionserver/start_services.sh
@@ -2,5 +2,5 @@ cd app/
 # Start rasa actions server 
 # rasa run actions --actions app.actions --enable-api --cors "*" --debug \
 #          -p $PORT
-rasa run actions --enable-api --cors "*" --debug -p $PORT
+rasa run actions --cors "*" --debug -p $PORT
             
