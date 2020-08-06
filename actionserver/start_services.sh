cb640d846be160b94af57624810309ea99142840
Ajju2211 <azharuddinm2211@gmail.com>
2020-08-07 00:39:13 +0530

    Using spacy

--- actionserver/start_services.sh
+++ actionserver/start_services.sh
@@ -1,6 +1,6 @@
 cd app/
 # Start rasa actions server 
-# rasa run actions --actions app.actions --enable-api --cors "*" --debug \
+# rasa run actions --actions app.actions  --cors "*" --debug \
 #          -p $PORT
-rasa run actions --cors "*" --debug -p $PORT
+rasa run actions --actions actions --cors "*" --debug -p $PORT
             
