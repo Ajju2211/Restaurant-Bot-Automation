83edb29bce33f341f772f7f64c91909bad529792
Ajju2211 <azharuddinm2211@gmail.com>
2020-10-02 18:51:23 +0530

    working fine

--- custom_tracker.py
+++ custom_tracker.py
@@ -6,8 +6,7 @@ import logging
 import os
 import pickle
 from datetime import datetime, timezone
-# Import custom module made for firebase credentials
-from actionserver.db.dbConfig import db
+
 
 from time import sleep
 from typing import (
@@ -41,23 +40,20 @@ from rasa.utils.endpoints import EndpointConfig
 import sqlalchemy as sa
 from rasa.core.tracker_store import TrackerStore
 
-logger = logging.getLogger(__name__)
+# Import custom dbConfig for firebase returns firestore.client() class object
+from actionserver.db.dbConfig import db
 
+logger = logging.getLogger(__name__)
+# COLLECTION = "restaurant-bot-tracker"
 
 
 class FirebaseTrackerStore(TrackerStore):
-    """Stores conversation history in memory"""
+    """Stores conversation history in Firebase"""
 
-    # def __init__(
-    #     self, domain: Domain, event_broker: Optional[EventBroker] = None
-    # ):
     def __init__(
         self,
         domain: Domain,
-<<<<<<< HEAD
         collection: Optional[Text] = "tracker",
-=======
->>>>>>> 2ef12546cd268709be6fdf654b962ea9093d5152
         host: Optional[Text] = "localhost",
         event_broker: Optional[EventBroker] = None
     ):
@@ -66,7 +62,6 @@ class FirebaseTrackerStore(TrackerStore):
         super().__init__(domain, event_broker)
 
     def save(self, tracker: DialogueStateTracker) -> None:
-<<<<<<< HEAD
         """Updates and saves the current conversation state
 
         Args:
@@ -86,21 +81,6 @@ class FirebaseTrackerStore(TrackerStore):
             ref = db.collection(self.COLLECTION).document(tracker.sender_id)
             ref.set(serialised)
 
-=======
-        """Updates and saves the current conversation state"""
-
-        try:
-            if self.event_broker:
-                self.stream_events(tracker)
-            serialised = self.serialiseTracker(tracker)
-
-
-            # self.store[tracker.sender_id] = serialised
-            # store to Firestore
-            ref = db.collection(COLLECTION).document(tracker.sender_id)
-            ref.set(serialised)
-
->>>>>>> 2ef12546cd268709be6fdf654b962ea9093d5152
         except Exception as e:
             traceback.print_exc()
 
@@ -118,7 +98,6 @@ class FirebaseTrackerStore(TrackerStore):
         check = db.collection(self.COLLECTION).document(sender_id).get().exists
         return check
 
-    
     def retrieve(self, sender_id: Text) -> Optional[DialogueStateTracker]:
         """
         Args:
@@ -138,23 +117,16 @@ class FirebaseTrackerStore(TrackerStore):
             return None
 
     def keys(self) -> Iterable[Text]:
-<<<<<<< HEAD
         """
         Returns: 
             sender_ids of the Tracker Store in Firebase
         """
 
         docs = db.collection(self.COLLECTION).stream()
-=======
-        """Returns sender_ids of the Tracker Store in Firebase"""
-        
-        docs = db.collection(COLLECTION).stream()
->>>>>>> 2ef12546cd268709be6fdf654b962ea9093d5152
         keys = []
         for doc in docs:
             keys.append(doc.id)
         return keys
-<<<<<<< HEAD
 
     def serialiseTracker(self, tracker:DialogueStateTracker):
         """
@@ -167,13 +139,6 @@ class FirebaseTrackerStore(TrackerStore):
         """
         try:
             dialogue = tracker.as_dialogue().as_dict()
-=======
-    def serialiseTracker(self, tracker):
-        """User defined serialisation"""
-        try:
-            dialogue = tracker.as_dialogue().as_dict()
-            print(f'dialoggue: {dialogue}')
->>>>>>> 2ef12546cd268709be6fdf654b962ea9093d5152
             dialogue = json.dumps(dialogue)
             dialogue = json.loads(dialogue)
             return dialogue
@@ -182,7 +147,6 @@ class FirebaseTrackerStore(TrackerStore):
             return None
 
     def deserialiseTracker(self, sender_id):
-<<<<<<< HEAD
         """User defined deserialisation
         Args:
             sender_id : Takes sender_id as input
@@ -191,38 +155,15 @@ class FirebaseTrackerStore(TrackerStore):
             returns tracker
         """
         
-=======
-        """User defined deserialisation"""
-        # dialogue = pickle.loads(_json)
-        # try:
-        #     ref = db.collection(COLLECTION).document(sender_id)
-        #     dialogue = ref.get().to_dict()
-        #     tracker = self.init_tracker(sender_id)
-        #     tracker.recreate_from_dialogue(dialogue)
-        #     return tracker
-        # except Exception as e:
-        #     logger.error('Sender Id is not found')
-        #     return None
-        # tracker = self.init_tracker(sender_id)
-        # if not tracker:
-        #     return None
->>>>>>> 2ef12546cd268709be6fdf654b962ea9093d5152
         tracker = self.init_tracker(sender_id)
         try:
             if not tracker:
                 return None
-<<<<<<< HEAD
             ref = db.collection(self.COLLECTION).document(sender_id)
             dialogue = ref.get().to_dict()
             # serialiseTracker(dialogue) no need to pass
             dialogue = Dialogue.from_parameters(
                 json.loads(json.dumps(dialogue)))
-=======
-            ref = db.collection(COLLECTION).document(sender_id)
-            dialogue = ref.get().to_dict()
-            # serialiseTracker(dialogue) no need to pass
-            dialogue = Dialogue.from_parameters(json.loads(json.dumps(dialogue)))
->>>>>>> 2ef12546cd268709be6fdf654b962ea9093d5152
         except Exception as e:
             traceback.print_exc()
         tracker.recreate_from_dialogue(dialogue)
